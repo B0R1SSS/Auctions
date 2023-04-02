7a4f653926733fd65dc71d27d017d9995ec850ba
Boris Savov <borko.savov@azet.sk>
2023-04-02 13:23:11 +0200

    feat[v02]: setup admin interface

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import RegisteredUser, Auction, Bid, Comment, Category
 
-# Register your models here.
+
+admin.site.register(RegisteredUser)
+admin.site.register(Auction)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Category)
--- auctions/urls.py
+++ auctions/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from django.contrib import admin
 from . import views
 
+app_name = "auctions"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("admin_interface", admin.site.urls, name="admin_interface")
 ]
