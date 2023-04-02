499af134c044f0b725a2f2ede439641ea5e4e189
Boris Savov <borko.savov@azet.sk>
2023-04-02 12:49:54 +0200

    feat[v01]: created models

--- auctions/models.py
+++ auctions/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class RegisteredUser(AbstractUser):
+    def __str__(self):
+        return f"User {self.id}: {self.username}"
+
+
+class Category(models.Model):
+    name =  models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"Category: {self.name}"
+
+
+class Bid(models.Model):
+    user = models.ForeignKey(RegisteredUser, on_delete=models.CASCADE, related_name="bids")
+    value = models.FloatField()
+
+    def __str__(self):
+        return f"Bid ${self.value} placed by {self.user} on {self.auction.get()}"
+
+
+class Auction(models.Model):
+    user = models.ForeignKey(RegisteredUser, on_delete=models.CASCADE, related_name="owned_auctions", default=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="auctions", null=True)
+    heighestBid = models.ForeignKey(Bid, on_delete=models.SET_NULL, related_name="auction", unique=True, null=True)
+    watchlist = models.ManyToManyField(RegisteredUser, related_name="watchlist_auctions")
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=500, blank=True)
+    starting_bid = models.FloatField()
+    image_url = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"Auction: {self.title}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(RegisteredUser, on_delete=models.DO_NOTHING, related_name="comments")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comments")
+    content = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"Comment on {self.auction.title} by {self.user}"
+
