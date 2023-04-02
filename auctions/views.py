49e55ff2ba45e2c0dfcd7065d19e5cddef773d53
Boris Savov <borko.savov@azet.sk>
2023-04-02 11:05:00 +0200

    initial commit

---
+++ auctions/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    return render(request, "auctions/index.html")
