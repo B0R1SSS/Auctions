40cf50364350c8dc07f9c934aef1d7e5347ebd04
Boris Savov <borko.savov@azet.sk>
2023-04-02 19:54:05 +0200

    feat[v02]: implemented views and templates for register,login and logout

--- auctions/urls.py
+++ auctions/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "auctions"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("admin_interface", admin.site.urls, name="admin_interface")
+    path("", views.index_view, name="index"),
+    path("register", views.register_view, name="register"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -4,7 +4,64 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from .models import RegisteredUser, Auction, Bid, Comment, Category
 
 
-def index(request):
-    return render(request, "auctions/index.html")
+def index_view(request):
+    auctions = Auction.objects.all()
+    return render(request, "auctions/index.html", {
+        "auctions": auctions
+    })
+
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = RegisteredUser.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    elif request.method == "GET":
+        return render(request, "auctions/register.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("auctions:index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    elif request.method == "GET":
+        return render(request, "auctions/login.html")
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("auctions:index"))
+
